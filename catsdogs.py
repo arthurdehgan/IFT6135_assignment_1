768cd5095420a55d4337052a72d7b062cc9762b7
arthurdehgan <arthurdehgan@github.com>
2019-02-12 09:24:37 -0500

    hyperparameter optimization

--- catsdogs.py
+++ catsdogs.py
@@ -1,69 +1,15 @@
-import math
 import numpy as np
 import torch
 from torch import nn
 from torch import optim
 import torch.utils.data as utils
-from params import batch_size, store_every, n_epochs, DATA_PATH, learning_rate
-from sklearn.model_selection import train_test_split
+from params import batch_size, DATA_PATH
 
 
 class Flatten(nn.Module):
     def forward(self, X):
         X = X.view(X.size(0), -1)
         return X
-
-
-class Dropout(nn.Module):
-    def forward(self, X, p=0.2):
-        shape = X.shape
-        size = int(p * np.prod(shape))
-        drop_idx = np.random.choice(np.arange(X.size), replace=False, size=size)
-        X = X.flatten()
-        X[drop_idx] = 0
-        return X.reshape(shape)
-
-
-class NN(nn.Module):
-    def __init__(self, features):
-        super(NN, self).__init__()
-
-        self.features = features
-        for m in self.modules():
-            if isinstance(m, nn.Conv2d):
-                n = m.kernel_size[0] * m.kernel_size[1] * m.out_channels
-                m.weight.data.normal_(0, math.sqrt(2.0 / n))
-                m.bias.data.zero_()
-
-    def forward(self, X):
-        X = self.features(X)
-        return X
-
-
-model = nn.Sequential(
-    nn.Conv2d(3, 64, 3, 1),
-    nn.ReLU(True),
-    nn.MaxPool2d(2, 2),
-    nn.Conv2d(64, 128, 3, 1),
-    nn.ReLU(True),
-    nn.MaxPool2d(2, 2),
-    nn.Conv2d(128, 256, 3, 1),
-    nn.MaxPool2d(2, 2),
-    nn.ReLU(True),
-    nn.Conv2d(256, 512, 3, 1),
-    nn.MaxPool2d(2, 2),
-    nn.ReLU(True),
-    Flatten(),
-    nn.Linear(2048, 512),
-    nn.ReLU(True),
-    nn.Linear(512, 512),
-    nn.ReLU(True),
-    nn.Linear(512, 2),
-)
-
-model = model.cuda()
-criterion = nn.CrossEntropyLoss()
-optimizer = optim.SGD(model.parameters(), lr=learning_rate)
 
 
 def accuracy(y_pred, target):
@@ -95,16 +41,18 @@ def evaluate(dataloader, batch_size=batch_size):
     return floss, faccuracy
 
 
-def train(dataloader, testloader):
-    LOSSES = 0
-    COUNTER = 0
-    ITERATIONS = 0
-    avg_loss = float("inf")
+def train(dataloader, testloader, lr):
+    GO = True
+    STOP = 0
+    EPOCH = 0
     train_accs = []
-    test_accs = []
+    valid_accs = []
     train_losses = []
-    test_losses = []
-    for e in range(n_epochs):
+    valid_losses = []
+    valid_loss = float("inf")
+    best_model = {"vacc": 0}
+    while GO:
+        EPOCH += 1
         for batch in dataloader:
             optimizer.zero_grad()
             X, y = batch
@@ -117,28 +65,42 @@ def train(dataloader, testloader):
             loss.backward()
             optimizer.step()
 
-            n = y.size(0)
-            LOSSES += loss.sum().data.cpu().numpy() * n
-            COUNTER += n
-            ITERATIONS += 1
-            if ITERATIONS % (store_every / 2) == 0:
-                avg_loss = LOSSES / float(COUNTER)
-                LOSSES = 0
-                COUNTER = 0
-                print(" Iteration {}: TRAIN {}".format(ITERATIONS, avg_loss))
+        old_valid_loss = valid_loss
+        train_loss, train_acc = evaluate(dataloader)
+        valid_loss, valid_acc = evaluate(validloader)
 
-            if ITERATIONS % (store_every) == 0:
+        train_accs.append(train_acc)
+        train_losses.append(train_loss)
+        valid_losses.append(valid_loss)
+        valid_accs.append(valid_acc)
+        if valid_acc > best_model["vacc"]:
+            model.cpu()
+            best_model = {
+                "model": model,
+                "epoch": EPOCH,
+                "batch_size": batch_size,
+                "lr": lr,
+                "vacc": valid_acc,
+                "vloss": valid_losses,
+                "taccs": train_accs,
+                "tloss": train_losses,
+                "vaccs": valid_accs,
+            }
+            model.cuda()
+        if valid_loss > old_valid_loss:
+            STOP += 1
+            if STOP > 2:
+                GO = False
+        else:
+            STOP = 0
 
-                train_loss, train_acc = evaluate(dataloader)
-                train_accs.append(train_acc)
-                train_losses.append(train_loss)
-                test_loss, test_acc = evaluate(testloader)
-                test_accs.append(test_acc)
-                test_losses.append(test_loss)
-
-                print(" [EPOCH] {}".format(e + 1))
-                print(" [NLL] TRAIN {} / TEST {}".format(train_loss, test_loss))
-                print(" [ACC] TRAIN {} / TEST {}".format(train_acc, test_acc))
+        print("EPOCH: {}".format(EPOCH))
+        print(" [NLL] TRAIN {} / TEST {}".format(train_loss, valid_loss))
+        print(" [ACC] TRAIN {} / TEST {}".format(train_acc, valid_acc))
+    np.save(
+        "best_model_{}_{}_{:.2f}".format(batch_size, lr, best_model["vacc"]), best_model
+    )
+    print("stopping, best valid acc is:", best_model["vacc"])
 
 
 if __name__ == "__main__":
@@ -148,22 +110,53 @@ if __name__ == "__main__":
     X = X[idx]
     y = np.load(DATA_PATH + "train_target.npy")
     y = y[idx]
-    # validation_set = np.load(DATA_PATH + "test_data.npy")
+    # validation_set = np.load(DATA_PATH + "valid_data.npy")
     # validation = torch.Tensor(validation_set).float()
 
     X = torch.Tensor(X).float()
     y = torch.Tensor(y).long()
     train_size = int(0.8 * len(X))
-    test_size = len(X) - train_size
-    train_index, test_index = torch.utils.data.random_split(
-        np.arange(len(X)), [train_size, test_size]
+    valid_size = len(X) - train_size
+    train_index, valid_index = torch.utils.data.random_split(
+        np.arange(len(X)), [train_size, valid_size]
     )
     train_dataset = utils.TensorDataset(X[train_index], y[train_index])
     dataloader = utils.DataLoader(
         train_dataset, batch_size=batch_size, shuffle=True, num_workers=2
     )
-    test_dataset = utils.TensorDataset(X[test_index], y[test_index])
-    testloader = utils.DataLoader(
-        test_dataset, batch_size=batch_size, shuffle=True, num_workers=2
+    valid_dataset = utils.TensorDataset(X[valid_index], y[valid_index])
+    validloader = utils.DataLoader(
+        valid_dataset, batch_size=batch_size, shuffle=True, num_workers=2
     )
-    train(dataloader, testloader)
+    lrs = [0.05, 0.01, 0.005]
+    for lr in lrs:
+        model = nn.Sequential(
+            nn.Conv2d(3, 64, 3, 1),
+            nn.ReLU(True),
+            nn.Conv2d(64, 128, 3, 1),
+            nn.ReLU(True),
+            nn.Conv2d(128, 128, 3, 1),
+            nn.ReLU(True),
+            nn.MaxPool2d(2, 2),
+            nn.Conv2d(128, 256, 3, 1),
+            nn.ReLU(True),
+            nn.Conv2d(256, 256, 3, 1),
+            nn.ReLU(True),
+            nn.MaxPool2d(2, 2),
+            nn.Conv2d(256, 512, 3, 1),
+            nn.ReLU(True),
+            nn.Conv2d(512, 512, 3, 1),
+            nn.ReLU(True),
+            nn.MaxPool2d(2, 2),
+            Flatten(),
+            nn.Linear(8192, 512),
+            nn.ReLU(True),
+            nn.Linear(512, 512),
+            nn.ReLU(True),
+            nn.Linear(512, 2),
+        )
+
+        model = model.cuda()
+        criterion = nn.CrossEntropyLoss()
+        optimizer = optim.SGD(model.parameters(), lr=lr)
+        train(dataloader, validloader, lr)
