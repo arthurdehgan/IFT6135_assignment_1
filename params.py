e5ffcc9f39c93b857a9c86e1024dbc6cadb9f25d
arthurdehgan <arthurdehgan@github.com>
2019-02-15 10:36:00 -0500

    cleaning up

--- params.py
+++
@@ -1,9 +0,0 @@
-TRAIN_SET = "/home/arthur/data/Kaggle/trainset/"
-TEST_SET = "/home/arthur/data/Kaggle/testset/"
-DATA_PATH = "/home/arthur/data/Kaggle/"
-SUBMISSION = "/home/arthur/data/Kaggle/sample_submission.csv"
-
-learning_rate = 0.1
-n_epochs = 100
-batch_size = 64
-store_every = 1000
